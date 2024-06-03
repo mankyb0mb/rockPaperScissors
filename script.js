8262b23d81b2feac250276266667be8744b3ac7a
mankyb0mb <rainerb0716@gmail.com>
2024-06-03 16:47:19 -0700

    pain and suffering

--- script.js
+++ script.js
@@ -3,6 +3,94 @@ var playerScore = 0
 var computerScore = 0
 var ties = 0
 
+const domScore = document.querySelector("p.playerScore")
+const domCpu = document.querySelector("p.cpuScore")
+const domTie = document.querySelector("p.ties")
+const domLog = document.querySelector("p.log")
+const domWin = document.querySelector("p.winner")
+
+domScoreHeld = domScore.innerHTML
+domScore.innerHTML = domScoreHeld + playerScore
+
+domCpuHeld = domCpu.innerHTML
+domCpu.innerHTML = domCpuHeld + computerScore
+
+domTieHeld = domTie.innerHTML
+domTie.innerHTML = domTieHeld + ties
+
+
+const btnRock = document.querySelector("button.rock")
+const btnPaper = document.querySelector("button.paper")
+const btnScissors = document.querySelector("button.scissors")
+const btnReset = document.querySelector("button.reset")
+
+choiceiguess = ""
+
+function resetter(){
+    playerScore = 0
+    computerScore = 0
+    ties = 0
+    domScore.innerHTML = domScoreHeld + playerScore 
+    domCpu.innerHTML = domCpuHeld + computerScore
+    domTie.innerHTML = domTieHeld + ties
+    domLog.innerHTML = ""
+    domWin.innerHTML = ""
+}
+
+const neededWins = 8
+
+btnRock.addEventListener("click", function(){
+    choiceiguess = btnRock.textContent.toUpperCase()
+    console.log("Rock Pressed")
+    if (playerScore == neededWins || computerScore == neededWins) {
+        resetter()
+    }
+    else if (playerScore != neededWins || computerScore != neededWins) {
+        playRound()
+        if (playerScore == neededWins) {
+            domWin.innerHTML = "YOU WON!!!!"
+        }
+        else if (computerScore == 5) {
+            domWin.innerHTML = "YOU LOST!!!!"
+        }
+    }
+})
+btnPaper.addEventListener("click", function(){
+    choiceiguess = btnPaper.textContent.toUpperCase()
+    console.log("Paper Pressed")
+    if (playerScore == 5 || computerScore == 5) {
+        resetter()
+    }
+    else if (playerScore != 5 || computerScore != 5) {
+        playRound()
+        if (playerScore == 5) {
+            domWin.innerHTML = "YOU WON!!!!"
+        }
+        else if (computerScore == 5) {
+            domWin.innerHTML = "YOU LOST!!!!"
+        }
+    }
+})
+btnScissors.addEventListener("click", function(){
+    choiceiguess = btnScissors.textContent.toUpperCase()
+    console.log("Scissors Pressed")
+    if (playerScore == 5 || computerScore == 5) {
+        resetter()
+    }
+    else if (playerScore != 5 || computerScore != 5) {
+        playRound()
+        if (playerScore == 5) {
+            domWin.innerHTML = "YOU WON!!!!"
+        }
+        else if (computerScore == 5) {
+            domWin.innerHTML = "YOU LOST!!!!"
+        }
+    }
+})
+btnReset.addEventListener("click", function(){resetter()})
+
+
+
 function getComputerChoice() {
     var choice = (Math.random() * 10)
     if (choice < 4) {
@@ -17,37 +105,54 @@ function getComputerChoice() {
     return choice
 }
 
-function getHumanChoice() {
-    var bruh
-    while (bruh != true) {
-        var userChoice = prompt("Rock, Paper, or Scissors?")
-        userChoice = userChoice.toUpperCase()
-        for (let i = 0; i < hands.length; i++) {
-            if (userChoice == hands[i]) {
-                bruh = true
+function getHumanChoice(button = true) {
+    if (!button) {
+        var bruh
+        while (bruh != true) {
+            var userChoice = prompt("Rock, Paper, or Scissors?")
+            userChoice = userChoice.toUpperCase()
+            for (let i = 0; i < hands.length; i++) {
+                if (userChoice == hands[i]) {
+                    bruh = true
+                }
+            }
+            if (bruh != true) {
+            console.log("Invalid Hand. Try Again!")
             }
         }
-        if (bruh != true) {
-        console.log("Invalid Hand. Try Again!")
-        }
+        return userChoice
     }
-    return userChoice
+    else {
+        return choiceiguess
+    }
 
 }
 
 // plays the round
-function playRound() {
+function playRound(button = false) {
     function winMsg() {
         console.log(human + " beats " + comp + ". You win!")
+        domLog.innerHTML = human + "{YOU}" + " beats " + comp + "{COMP}" + ". You win!"
         playerScore++
+        domScore.innerHTML = domScoreHeld + playerScore
+    }
+    function loseMsg() {
+        console.log(comp + " beats " + human + ". You lose!")
+        domLog.innerHTML = comp + "{COMP}" + " beats " + human + "{YOU}" + ". You lose!"
+        computerScore++
+        domCpu.innerHTML = domCpuHeld + computerScore
     }
     var comp = getComputerChoice()
     var human = getHumanChoice()
+
+
     console.log("Computer choice: " + comp)
     console.log("Human choice: " + human)
     if (human == comp) {
         console.log("Tie!")
+        domLog.innerHTML = "Tie!"
         ties++
+        domTie.innerHTML = domTieHeld + ties
     }
     else if ((human == hands[0]) && (comp == hands[2])) {
         winMsg()
@@ -59,8 +164,7 @@ function playRound() {
         winMsg()
     }
     else {
-        console.log(comp + " beats " + human + ". You lose!")
-        computerScore++
+        loseMsg()
     }
     console.log(`Player : ${playerScore}
 Computer : ${computerScore}
@@ -73,5 +177,6 @@ function playGame(rounds = 1) {
         playRound()
     }
 }
+
 
 
